6f97e61dfa92de6826c731886e7658cd03618c51
VWILLIAM <v.workzone@outlook.com>
2019-05-08 14:15:09 -1000

    Ajout des Programmes - Code / fonction

---
+++ Programme/Code.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 14:11:54 2019
+
+@author: vwork
+--Main Code--
+"""
+
---
+++ Programme/fonction.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 14:11:54 2019
+
+@author: vwork
+"""
+
